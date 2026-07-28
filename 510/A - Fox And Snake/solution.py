c048fd327fb58e474047f9c572b4d6fd9966895d
Adabala Raghu Ram <raghuram829729@gmail.com>
2026-07-28 23:49:41 +0530

    Add Fox And Snake [A] from Codeforces

---
+++ 510/A - Fox And Snake/solution.py
@@ -0,0 +1,17 @@
+import sys
+ 
+input = sys.stdin.readline
+ 
+def main():
+    n,m = map(int,input().split())
+ 
+    for i in range(n):
+        if i%2==0:
+            print("#"*m)
+        elif i%4==1:
+            print("."*(m-1)+"#")
+        else:
+            print("#"+"."*(m-1))
+ 
+if __name__ == "__main__":
+    main()

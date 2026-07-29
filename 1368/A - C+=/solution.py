3fc3f0fe49fb2b41e680dfdeedeff2244a7a169b
Adabala Raghu Ram <raghuram829729@gmail.com>
2026-07-30 02:21:16 +0530

    Add C+= [A] from Codeforces

---
+++ 1368/A - C+=/solution.py
@@ -0,0 +1,20 @@
+import sys
+ 
+input = sys.stdin.readline
+ 
+def main():
+    t = int(input())
+    for _ in range(t):
+        a,b,n=map(int,input().split())
+        ans=0
+        while max(a,b)<=n:
+            if a<b:
+                a+=b
+            else:
+                b+=a
+            ans+=1
+        print(ans)
+            
+ 
+if __name__ == "__main__":
+    main()
